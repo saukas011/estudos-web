d28b640b57040f6e8771c441c6f282e569900a40
saukas011 <franc2reis@gmail.com>
2026-02-07 16:46:03 -0300

    add barra de busca funcional
    código bem poluído a ser otimizado

--- js/projetos/weather-app/assets/weather.js
+++ js/projetos/weather-app/assets/weather.js
@@ -3,19 +3,36 @@ function weatherApp(){
     const referenceTempEl = document.querySelector(".reference-temp");
     const windSpeedEl = document.querySelector(".wind-speed");
     const humidityEl = document.querySelector(".humidity");
-    const greaterEl = document.querySelector(".city-list");
-    const candidateCities = [
-        "Rio de Janeiro", "Fortaleza", "Cuiabá", "Dubai", "Bangkok", 
-        "Miami", "Cairo", "Riyadh", "Singapore", "Jakarta", 
-        "Manaus", "New Delhi", "Doha", "Phoenix", "Las Vegas"
+    const greaterCitiesEl = document.querySelector(".city-list");
+    const standartCityEl = document.querySelector(".standart-city");
+    const searchEngine = document.querySelector("#search-input");
+    const suggestionsEl = document.querySelector(".suggestions");
+    const searchIcon = document.querySelector(".search-icon")
+    const candidateCities = ["Rio de Janeiro", "Dubai", "Bangkok", "Las Vegas"];
+    const availableCities = [
+        "Abu Dhabi", "Amsterdam", "Athens", "Auckland", "Bali", "Bangkok", "Barcelona", "Beijing",
+        "Belo Horizonte", "Berlin", "Bogota", "Bora Bora", "Boston", "Brasília", "Brisbane", "Brussels", 
+        "Budapest", "Buenos Aires", "Cairo", "Cancun", "Cape Town", "Cartagena", "Casablanca", "Chicago", 
+        "Copenhagen", "Curitiba", "Cusco", "Doha", "Dubai", "Dublin", "Edinburgh", "Florence", "Fortaleza", 
+        "Geneva", "Hanoi", "Havana", "Ho Chi Minh City", "Hong Kong", "Honolulu","Istanbul", "Jakarta", 
+        "Jerusalem", "Johannesburg", "Kuala Lumpur", "Kyoto", "Lagos", "Las Vegas", "Lima", "Lisbon", 
+        "Liverpool", "London", "Los Angeles", "Macau", "Madrid", "Manchester", "Manaus", "Manila", 
+        "Marrakech", "Medellin", "Melbourne", "Mexico City", "Miami", "Milan", "Monaco", "Montevideo", 
+        "Montreal", "Moscow", "Mumbai", "Munich", "Nairobi", "Naples", "New Delhi", "New York", "Orlando", 
+        "Osaka", "Oslo", "Panama City", "Paris", "Perth", "Petra", "Phuket", "Porto", "Prague", "Quito", 
+        "Recife", "Reykjavik", "Rio de Janeiro", "Riyadh", "Rome", "Salvador", "San Francisco", "Santiago", 
+        "Santorini", "São Paulo", "Seattle", "Seoul", "Seville", "Shanghai", "Shenzhen", "Singapore", 
+        "Stockholm", "Sydney", "Taipei", "Tel Aviv", "Tokyo", "Toronto", "Vancouver", "Venice", "Vienna", 
+        "Warsaw", "Washington", "Zanzibar", "Zurich"
     ];
 
     async function getWeather(city){
         try{
             //coloque sua key aqui! para cria-la, acesse openweathermap.org
-            const apiKey = process.env.API_KEY;
+            //const apiKey = process.env.API_KEY;
+            const apiKey = '4b9d929503af7af2e14c5bdd0626e1a0';
 
-            let coords = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city},BR&limit=1&appid=${apiKey}`);
+            let coords = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`);
             if(!coords){
                 throw new Error("erro ao buscar cidade");
             }
@@ -26,24 +43,25 @@ function weatherApp(){
             const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=4b9d929503af7af2e14c5bdd0626e1a0&units=metric`);
             const data = await res.json();
             if(!data) throw new Error("erro ao buscar informações");
-            greaterEl.innerHTML += '<p>carregando...</p>'
+            standartCityEl.innerHTML = city;
             tempEl.innerHTML = `${Math.round(data.main.temp)}°C`;
             referenceTempEl.innerHTML = `${Math.round(data.main.temp)}°C`;
             windSpeedEl.innerHTML = `Vento: ${(data.wind.speed).toFixed(1)}m/s`
             humidityEl.innerHTML = `Umidade: ${data.main.humidity}%`
+            greaterCitiesEl.innerHTML = '<p>carregando...</p>'
             
             const referenceTemp = Math.round(data.main.temp);
-            getCitiesGreaterThanTemp(candidateCities, apiKey, referenceTemp);
+            getCitiesGreaterThanTemp(availableCities, apiKey, referenceTemp);
         }catch(err){
             console.error(err);
         }
-    }
-    getWeather('Joinville');
+    } 
+    getWeather('São Paulo');
 
-    async function getCitiesGreaterThanTemp(candidateCities, apiKey, referenceTemp){
+    async function getCitiesGreaterThanTemp(availableCities, apiKey, referenceTemp){
         try{
             const foundCities = [];
-            for(let city of candidateCities){
+            for(let city of availableCities){
                 let coords = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`);
                 coords = await coords.json();
                 if(!coords) throw new Error("erro ao buscar coordenadas de cidades extras");
@@ -62,15 +80,71 @@ function weatherApp(){
         }
     }
     function addCitiesToDOM(foundCities){
-        let cityList = '';
-        for(let item of foundCities){
-            cityList += `<p class="city">${item[0]}..................${Math.ceil(item[1])}°C</p>`;
-        }
-        greaterEl.innerHTML = cityList;
+        let greaterCitiesList = '';
+
+        for(let item of foundCities)
+            greaterCitiesList += `<li class="city">${item[0]}..................${Math.ceil(item[1])}°C</li>`;
+
+        greaterCitiesEl.innerHTML = greaterCitiesList;
         if (foundCities.length === 0) {
-            greaterEl.innerHTML = "<p style='text-align:center'>Nenhuma cidade da lista é mais quente que aqui.</p>";
+            greaterCitiesEl.innerHTML = `<p style='text-align:center'>Nenhuma cidade da lista está mais quente que ${city}.</p>`;
             return;
         }
+    }
+
+    document.addEventListener('click', function(e){
+        const clickedElement = e.target;
+
+        if(clickedElement === searchEngine){
+            let suggestionsList = '';
+            for(let item of candidateCities)
+                suggestionsList += `<li class="search-item">${item}</li>`; 
+            
+            suggestionsEl.innerHTML = suggestionsList;
+            suggestionsEl.classList.add("selected");
+            searchEngine.classList.add("selected");
+            searchIcon.classList.add("selected");
+        }else{
+            suggestionsEl.classList.remove("selected");
+            searchEngine.classList.remove("selected");
+            searchIcon.classList.remove("selected");
+        }
+
+        if(clickedElement.classList.contains("search-item")){
+            getWeather(clickedElement.textContent.trim());
+        }
+    })
+
+    function search(input){
+        if(!input) return [];
+        const results = [];
+        const userInput = toSearchTerm(input);
+
+        for(let city of availableCities){
+            if(toSearchTerm(city).startsWith(userInput)) results.push(city);
+            if(results.length>=5) break;
+        }
+        return results;
+    }
+
+    
+    let timer = null;
+    searchEngine.addEventListener('input', function(e){
+        clearTimeout(timer);
+        suggestionsEl.innerHTML = "Buscando..."
+        timer = setTimeout(() => {
+            const results = search(searchEngine.value);
+            let matchListEl = '';
+            for(let item of results)
+                matchListEl += `<li class="search-item">${item}</li>`;
+
+            suggestionsEl.innerHTML = matchListEl;
+            if(!matchListEl) suggestionsEl.innerHTML = "Sem resultados para a busca";
+        }, 500);
+    });
+
+    function toSearchTerm(str) {
+        return str.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "");
     }
 }
 weatherApp();
