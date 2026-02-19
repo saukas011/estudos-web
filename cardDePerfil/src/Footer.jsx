2322deba5cca726237dacbff3c3f3358ca3394ac
saukas011 <franc2reis@gmail.com>
2026-02-19 18:36:33 -0300

    projeto simples finalizado

---
+++ cardDePerfil/src/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer(){
+    return(
+        <footer>
+            <p>Página Feita Com React | &copy; {new Date().getFullYear()} Francisco Saukas</p>
+        </footer>
+    );
+}
+
+export default Footer;
